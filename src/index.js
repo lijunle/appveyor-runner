73bdf1795d6a938adb2fc328a5489f257e371091
Junle Li <lijunle@gmail.com>
2016-06-21 23:56:29 +0800

    Enable ESLint and Airbnb-Base rules.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import path from 'path';
 
 export default async function main() {
   const dirname = await path.resolve(__dirname);
-  console.log(`This is file ${dirname}`);
+  console.log(`This is file ${dirname}`); // eslint-disable-line no-console
 }
