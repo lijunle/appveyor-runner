e1760d5b4c65ad7a4af4f8f87fd972389c9163cc
Junle Li <lijunle@gmail.com>
2016-06-25 17:44:43 +0800

    Redirect the runner output and assert on it.
    - Add tape.js helper to manage the output.
    - Do not pollute the test output.
    - Assert the command is run and output contains node version.
    - Pending to improve the test case.

--- test/basic.js
+++ test/basic.js
@@ -1,29 +1,23 @@
 import path from 'path';
-import mkdirp from 'mkdirp-promise';
-import tape from 'tape-promise/tape';
+import test from './tape';
 import runner from '../src/index';
 
 const binDir = path.resolve(__dirname, '../node_bin');
 
-function test(name, listener) {
-  tape(name, async (t) => {
-    const context = {};
-    try {
-      const now = Date.now().toString();
-      const logDir = path.resolve(__dirname, 'logs', now);
-      await mkdirp(logDir);
-      context.logDir = logDir;
-    } finally {
-      listener(t, context);
-    }
-  });
-}
+test('runner', async (t, context) => {
+  await runner(
+    context.stdout,
+    context.stderr,
+    binDir,
+    context.logDir,
+    ['6.2.2'],
+    ['node --version']
+  );
 
-test('runner', (t, context) => runner(
-  process.stdout,
-  process.stderr,
-  binDir,
-  context.logDir,
-  ['6.2.2'],
-  ['node --version', 'npm --version', 'more index.js >&2']
-));
+  const stdout = context.getStdout();
+  const stderr = context.getStderr();
+
+  t.includes(stdout, 'node --version');
+  t.includes(stdout, '6.2.2');
+  t.equal(stderr, '');
+});
