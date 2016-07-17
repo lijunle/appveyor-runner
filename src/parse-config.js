521f1d005367e612b29bc5b7616b994ca2acb715
Junle Li <lijunle@gmail.com>
2016-07-18 00:25:30 +0800

    Try to add JSDoc, but it does not work good.

--- src/parse-config.js
+++ src/parse-config.js
@@ -1,6 +1,11 @@
 import yaml from 'js-yaml';
 import fs from 'fs';
 
+/**
+ * Read the content from the file.
+ * @param {string} file The file path.
+ * @returns {string} The promise resolved with the file content.
+ */
 function readFile(file) {
   return new Promise((resolve, reject) =>
     fs.readFile(file, 'utf-8', (error, data) =>
@@ -9,6 +14,12 @@ function readFile(file) {
   );
 }
 
+/**
+ * Parse configuration from file.
+ * @export
+ * @param {string} configFile The configuration file path. It is YAML format.
+ * @returns {object} The configuration object.
+ */
 export default async function parseConfig(configFile) {
   const content = await readFile(configFile);
   const config = yaml.safeLoad(content);
